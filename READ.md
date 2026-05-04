86c07e0e51bc73543ffaed690d6a02f9ec405e78
Manuella <manufpinheiro1910@gmail.com>
2026-05-04 13:40:01 -0300

    first commit

---
+++ READ.md
@@ -0,0 +1,17 @@
+# Projeto 01 - Cartão de Visita
+
+## Manuella Ferreira Pinheiro
+
+Página pessoal desenvolvida com HTML, CSS e JavaScript puro.
+
+##  Tecnologias
+- HTML5
+- CSS3
+- JavaScript
+
+## Como rodar
+Abra o arquivo index.html no navegador.
+
+##  Links
+- LinkedIn: https://www.linkedin.com/in/manuella-ferreira-pinheiro-001aa3387/
+- GitHub: https://github.com/manufpinheiro
---
+++ script.js
@@ -0,0 +1,53 @@
+const toggle = document.getElementById("theme-toggle");
+
+toggle.onclick = () => {
+  document.body.classList.toggle("dark");
+
+  localStorage.setItem(
+    "theme",
+    document.body.classList.contains("dark") ? "dark" : "light"
+  );
+};
+
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
+
+
+const btn = document.getElementById("copy-email");
+const email = document.getElementById("email").innerText;
+const feedback = document.getElementById("feedback");
+
+btn.onclick = () => {
+  navigator.clipboard.writeText(email);
+  feedback.innerText = "Copiado!";
+  setTimeout(() => feedback.innerText = "", 2000);
+};
+
+
+const sections = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+sections.forEach(sec => observer.observe(sec));
+
+
+const text = "Estudante dedicada com foco em tecnologia e desenvolvimento contínuo.";
+let i = 0;
+
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("typewriter").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, 40);
+  }
+}
+
+typeWriter();
